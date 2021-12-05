1b378d46e4a961fe44d0bafaa52f084724a33ab6
developergurkan <gurkanatik35@gmail.com>
2021-12-06 00:33:09 +0300

    remove a todo

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
     data(){
         return{
-            todos : []
+            todos : [
+                {id: 1, text: 'learn vue', is_done: 0},
+                {id: 2, text: 'learn vue cli', is_done: 0},
+                {id: 3, text: 'learn laravel', is_done: 0},
+            ]
         }
     },
     methods:{
@@ -13,6 +17,21 @@ const app = Vue.createApp({
                 is_done : 0
             })
             todoInput.value = ''
+        },
+        remove(id){
+            this.todos = this.todos.filter((todo) => todo.id !== id)
+        },
+        complete(id){
+            let todoIndex = this.todos.findIndex(todo => todo.id === id);
+            this.todos[todoIndex].is_done = 1
+        }
+    },
+    computed:{
+        completedTodosCount(){
+            return this.todos.filter((todo) => todo.is_done).length;
+        },
+        unCompletedTodosCount(){
+            return this.todos.filter((todo) => !todo.is_done).length;
         }
     }
 }).mount('#app')
