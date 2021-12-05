4f11a758d979531e71a6ef217b45bcd70ecc189e
developergurkan <gurkanatik35@gmail.com>
2021-12-06 00:13:40 +0300

    add a todo

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 const app = Vue.createApp({
-
+    data(){
+        return{
+            todos : []
+        }
+    },
+    methods:{
+        add(){
+            let todoInput = document.getElementById('todo')
+            this.todos.push({
+                id: generateId(),
+                text : todoInput.value,
+                is_done : 0
+            })
+            todoInput.value = ''
+        }
+    }
 }).mount('#app')
+
+
+function generateId() {
+    return Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36);
+}
